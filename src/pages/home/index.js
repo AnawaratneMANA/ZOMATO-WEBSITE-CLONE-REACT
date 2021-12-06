7a5af6a01d4965de6e1e10f91a66c668dec075cf
AnawaratneMANA <it19086408@my.sliit.lk>
2021-12-06 02:04:17 -0500

    Switch Case Created.

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React , { useState } from 'react'
 import Footer from '../../components/common/footer';
 import Header from "../../components/common/header";
 import TabOptions from '../../components/common/tabOptions';
+
+
+
+const getCorrectScreen = (tab) => {
+    switch(tab){
+        case "Delivery":
+            return <div>Delivery</div>;
+        case "Dining Out":
+            return <div>Dining Out</div>;
+        case "NightLife":
+            return <div>Night Life</div>;
+        default: 
+            return <div>Delivery</div>;
+    }
+}
+
+
 const HomePage = () => {
+
+    const [activeTab, setActiveTab] = useState("Delivery");
+
     return (
 
         <div>
             <Header/>
             <TabOptions/>
-            {/* Footer Screen */}
+            {getCorrectScreen(activeTab)}
             <Footer/>
         </div>
     )
     
-}
+};
+
+
 
 export default HomePage;
---
+++ src/util/ReadMe.txt
@@ -0,0 +1,2 @@
+Link to the Video: https://www.youtube.com/watch?v=TDLc9AWEcFA
+Checkpoint: -
