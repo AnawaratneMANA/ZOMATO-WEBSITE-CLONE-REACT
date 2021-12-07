91c4b0539a6f24cec15ea9fe0bed6e86935aaa76
AnawaratneMANA <it19086408@my.sliit.lk>
2021-12-07 22:13:27 +0530

    Search Bar Styles Applied.

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -4,12 +4,14 @@ const Header = () => {
     return (
         <div className="max-width header">
             <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="zomato-logo" className="header-logo"/>
-            <div className="header-left">
+            <div className="header-right">
                 <div className="header-location-search-container">
                     <div className="location-wrapper">
                         <div className="location-icon-name">
-                        <i className="fi fi-rs-marker"></i>
+                        <i className="fi fi-rs-marker absolute-center location-icon"></i>
+                            <div> Sri Lanka</div>
                         </div>
+                        <i className="fi fi-rs-caret-down absolute-center"></i>
                     </div>
                  </div>
              </div>
