f7b3dcabd2858771959990f58b1bb9f5144f8b33
AnawaratneMANA <it19086408@my.sliit.lk>
2021-12-18 04:13:19 +0530

    Before adding constants - ExploreCard.

---
+++ src/components/exploreSection/exploreCard/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./exploreCard.scss"
+const ExploreCard  = ({restaurant}) => {
+
+    return (
+        <div>
+            This is explore card.
+        </div>
+    );
+}
+
+export default ExploreCard;
