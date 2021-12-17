5ca86a38dc2dd34967374d39335236831009a823
AnawaratneMANA <it19086408@my.sliit.lk>
2021-12-17 17:45:16 +0530

    Passed the Filter Array.

--- src/components/delivery/index.js
+++ src/components/delivery/index.js
@@ -2,13 +2,37 @@ import React from 'react'
 import "./delivery.scss";
 import Filters from "../common/filters";
 
+const deliveryFilters = [
+    {
+        id: 1,
+        icon: <i className="fi fi-rs-settings-sliders absolute-center"></i>,
+        title: "Filters"
+    },
+    {
+        id: 2,
+        title: "Rating"
+    },
+    {
+        id: 3,
+        title: "Safe Higenic"
+    },
+    {
+        id: 4,
+        title: "Pure Veg"
+    },
+    {
+        id: 5,
+        icon: <i className="fi fi-rs-apps-sort absolute-center"></i>,
+        title: "Delivery Time"
+    },
+]
 
 
 const Delivery = () => {
     return (
         <div>
             <div className="max-width">
-                <Filters/>
+                <Filters fitlerList={deliveryFilters}/>
             </div>
         </div>
 
