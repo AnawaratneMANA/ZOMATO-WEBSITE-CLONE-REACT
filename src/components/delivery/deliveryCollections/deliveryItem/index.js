a4b56888a98fe233cd0008ec6eb1efc34b52d397
AnawaratneMANA <it19086408@my.sliit.lk>
2021-12-17 18:35:12 +0530

    Item Images loaded.

--- src/components/delivery/deliveryCollections/deliveryItem/index.js
+++ src/components/delivery/deliveryCollections/deliveryItem/index.js
@@ -2,8 +2,8 @@ import React from "react"
 import "./deliveryItem.scss"
 const DeliveryItem = ({item}) => {
     return (
-        <div>
-            This is Delivery Item.
+        <div className="delivery-item-cover">
+            <img src={item.cover} className="delivery-item-image" alt={item.title}/>
         </div>
 
     );
